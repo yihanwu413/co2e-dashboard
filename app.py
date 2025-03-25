68a72e8b106811fa32ee15ff982e730f64e88e23
yihanwu413 <yihanwu413@gmail.com>
2025-03-25 10:11:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import io
 
 sns.set_theme(style="whitegrid", palette="coolwarm")
-sns.set_theme(style="whitegrid")
 st.set_page_config(page_title="CO₂e Emissions Dashboard", layout="wide")
 st.title("CO₂e Emissions Dashboard")
 
